1f3c959ba6fd8d9a586e0446a827be196c39f758
Daniel Heredia <rorhedia@gmail.com>
2022-04-27 07:28:46 -0500

    primer commit

---
+++ jsf-proyect1/index.js
@@ -0,0 +1,26 @@
+const express = require( 'express' );
+
+const app = express();
+const PORT = 4000;
+
+app.use( express.json() );
+
+app.get( '/', ( req, res ) => {
+    res.json( { Method: 'GET' } );
+} );
+
+app.post( '/usuarios', ( req, res ) => {
+    res.json( { Method: 'POST' } );
+} );
+
+app.put( '/', ( req, res ) => {
+    res.json( { Method: 'PUT' } );
+} );
+
+app.delete( '/', ( req, res ) => {
+    res.json( { Method: 'DELETE' } );
+} );
+
+app.listen( PORT, () => {
+    console.log( `Servidor ejecutandose en el puerto ${ PORT }` );
+} );
